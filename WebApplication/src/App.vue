e83221ba91a436d230a2bd30317a80718c860a73
David <desmaisons_david@hotmail.com>
2019-09-06 23:49:13 -0300

    Clean-up template

--- WebApplication/src/App.vue
+++ WebApplication/src/App.vue
@@ -1,36 +1,31 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <v-toolbar-title class="headline">
+        <v-icon>mdi-movie-roll</v-icon>
+        <span>Movie Explorer</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+import { watch } from "fs";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
   data: () => ({
-    //
+    dark: true
   }),
+  created() {
+    this.$vuetify.theme.dark = this.dark;
+  },
+  watch: {
+    dark(value) {
+      this.$vuetify.theme.dark = value;
+    }
+  }
 };
 </script>
