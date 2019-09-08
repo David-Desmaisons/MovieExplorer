6ccb1d4d1eddcfa81e46287278943d046bf0507e
David <desmaisons_david@hotmail.com>
2019-09-07 21:49:46 -0300

    Adding flipacd (check-behavior chrome vs ie)

---
+++ WebApplication/src/components/FlipCard.vue
@@ -0,0 +1,70 @@
+<template>
+<section class="card-container" :style="{width, height, perspective: perspective+'px'}">
+  <div class="flip-card" :class="{flipped}" :style="{transition: 'transform '+transition+'s'}">
+    <figure class="front">
+        <slot name="front">
+        </slot> 
+    </figure>
+    <figure class="back-card">
+        <slot name="back">
+        </slot>
+    </figure>
+  </div>
+</section>
+</template>
+<script>
+const props = {
+  flipped: {
+    required: false,
+    default: false
+  },
+  width: {
+    required: false,
+    type: String,
+    default: "150px"
+  },
+  height: {
+    required: false,
+    type: String,
+    default: "150px"
+  },
+  perspective: {
+    required: false,
+    type: Number,
+    default: 800
+  },
+  transition: {
+    required: false,
+    type: Number,
+    default: 1
+  }
+};
+export default {
+  props
+};
+</script>
+<style scoped>
+.card-container {
+  position: relative;
+}
+.flip-card {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  transform-style: preserve-3d;
+}
+.flip-card figure {
+  margin: 0;
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  /* backface-visibility: hidden; */
+}
+.flip-card .back-card {
+  transform: rotateY(180deg);
+}
+.flip-card.flipped {
+  transform: rotateY(180deg);
+}
+</style>
--- WebApplication/src/components/MovieCard.vue
+++ WebApplication/src/components/MovieCard.vue
@@ -1,17 +1,56 @@
 <template>
-  <v-card class="mx-auto" width="200px" height="400px">
-    <v-img height="100%" :src="image">
-    </v-img>
-  </v-card>
+  <FlipCard
+    @mouseover.native="flipped = true"
+    @mouseleave.native="flipped = false"
+    :flipped="flipped"
+    width="200px"
+    height="400px"
+  >
+    <template #front>
+      <!-- <v-img class="white--text" height="100%" :src="image" style="position:absolute;">
+      </v-img> -->
+      <div class="card-side" :style="{ background }"></div>
+    </template>
+
+    <template #back>
+      <div class="card-side back">
+        <div class="v-card__title">{{ title }}</div>
+        <div class="v-card__text">I'm card text</div>
+        <div class="v-card__actions">
+          <v-btn class="mx-2" fab dark small color="primary">
+            <v-icon dark>mdi-minus</v-icon>
+          </v-btn>
+        </div>
+      </div>
+      <!-- -->
+
+      <!--  <v-card class="card-side back">
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-text>I'm card text</v-card-text>
+       <v-card-actions class="actions">
+         <v-btn
+            class="actions"
+          >Click</v-btn>
+        </v-card-actions> 
+      </v-card>-->
+    </template>
+  </FlipCard>
 </template>
 <script>
+import FlipCard from "./FlipCard";
 export default {
+  components: {
+    FlipCard
+  },
+  data: () => ({
+    flipped: false
+  }),
   props: {
     image: {
       required: true,
       type: String
     },
-    title:{
+    title: {
       required: true,
       type: String
     }
@@ -32,5 +71,15 @@ export default {
 .image {
   width: 100%;
   height: 100%;
+}
+
+.card-side {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.back {
+  background: grey;
 }
 </style>
