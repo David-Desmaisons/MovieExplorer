c5c109b69227a98a714fe559555feef022f8043c
David <desmaisons_david@hotmail.com>
2019-09-08 21:20:48 -0300

    Clean ajax file

--- WebApplication/src/infra/ajax.js
+++ WebApplication/src/infra/ajax.js
@@ -10,21 +10,4 @@ async function get(url) {
   return response.data;
 }
 
-async function deleteMethod(url, data) {
-  const response = await axios.delete(`${getFullUrl(url)}/${data}`);
-  return response.data;
-}
-
-async function put(url, id, data) {
-  const response = await axios.put(`${getFullUrl(url)}/${id}`, data);
-  return response.data;
-}
-
-async function post(url, data) {
-  const response = await axios.post(getFullUrl(url), data, {
-    headers: { "Content-Type": "application/json" }
-  });
-  return response.data;
-}
-
-export { get, post, deleteMethod, put };
+export { get};
